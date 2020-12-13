9bfec4f57bd629d276490f278dc8fe9582b9019d
Matteo Lepur <lepurmatteo@gmail.com>
2020-12-12 18:07:32 -0800

    add case that produces numerical error

--- tests/testthat/test-normalize_log_probs.R
+++ tests/testthat/test-normalize_log_probs.R
@@ -7,5 +7,5 @@ test_that("test usage of normalize_log_probs function", {
   expect_equal(round(normalize_log_probs(c(-10, -10, -10, NA), na.rm=TRUE), 3), round(c(0.333, 0.333, 0.333), 3))
 
   # test values that produce numerically unstable values
-  expect_equal(round(normalize_log_probs(c(-100, -2, -1000)), 3), round(c(0, 1, 0), 3))
+  expect_error(normalize_log_probs(c(-1000, -1000, -1000)))
 })
